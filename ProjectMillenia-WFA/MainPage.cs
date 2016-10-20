eae503887ee97790fc4272e43fd3280d33651e4c
antho <antho@NAMELESS-SURFER>
2016-10-20 23:42:37 +0300

    FFT basic idea has been added to OpenAudio.cs constructor

--- ProjectMillenia-WFA/MainPage.cs
+++ ProjectMillenia-WFA/MainPage.cs
@@ -12,7 +12,6 @@ using System.Threading;
 using System.IO;
 using MidiToSheetMusic;
 using MidiSheetMusic;
-using SoundAnalysis;
 
 
 
--- ProjectMillenia-WFA/OpenAudio.cs
+++ ProjectMillenia-WFA/OpenAudio.cs
@@ -1,4 +1,5 @@
 ﻿using NAudio.Gui;
+using NAudio.Wave;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +14,8 @@ namespace ProjectMillenia_WFA
         public OpenFileDialog ChooseWav = new OpenFileDialog();
         public NAudio.Wave.WaveFileReader OpenedWave = null;
         public NAudio.Wave.DirectSoundOut WhereToPlayWav = null;
-      
+        
+        
 
         public OpenAudio()
         {
@@ -21,17 +23,44 @@ namespace ProjectMillenia_WFA
             if (ChooseWav.ShowDialog() != DialogResult.OK) return;
 
             DisposeWave();
-            
+
             OpenedWave = new NAudio.Wave.WaveFileReader(ChooseWav.FileName);
             WhereToPlayWav = new NAudio.Wave.DirectSoundOut();
             WhereToPlayWav.Init(new NAudio.Wave.WaveChannel32(OpenedWave));
-           
+
+
+
+            using (WaveFileReader reader = new WaveFileReader(ChooseWav.FileName))
+            {
+                IWaveProvider stream32 = new Wave16ToFloatProvider(reader);
+                IWaveProvider stream16 = new WaveFloatTo16Provider(stream32);
+                using (WaveFileWriter converted = new WaveFileWriter("C:/Millennia/test.wav", stream16.WaveFormat))
+                {
+                    // buffer length needs to be a power of 2 for FFT to work nicely
+                    // however, make the buffer too long and pitches aren't detected fast enough
+                    // successful buffer sizes: 8192, 4096, 2048, 1024
+                    // (some pitch detection algorithms need at least 2048)
+                    byte[] buffer = new byte[8192];
+                    int bytesRead;
+                    do
+                    {
+                        bytesRead = stream16.Read(buffer, 0, buffer.Length);
+                        converted.Write(buffer, 0, bytesRead);
+                    } while (bytesRead != 0 && converted.Length < reader.Length);
+                }MessageBox.Show("FFT has been applied here, but it has not been implemented yet!");
+            }
+
             /*
              * OpenedWave is used to work with the opened wav file. WhereToPlayWav is just the output device
              * In this constructor I am first choosing the type of the dialog opened (wav)
              * Then i am disposing of any file that was created before. Then the wav file is chosen
              * After that the wav file is just transfered to the direct output
              */
+
+
+
+
+
         }
 
         public void Play()
@@ -48,8 +77,8 @@ namespace ProjectMillenia_WFA
 
         public void draw(CustomWaveViewer DrawWave)
         {
-           DrawWave.WaveStream = new NAudio.Wave.WaveFileReader(ChooseWav.FileName);
-           DrawWave.FitToScreen();
+            DrawWave.WaveStream = new NAudio.Wave.WaveFileReader(ChooseWav.FileName);
+            DrawWave.FitToScreen();
         }
 
         public void UnDraw(CustomWaveViewer WaveToUnDraw)
@@ -75,5 +104,12 @@ namespace ProjectMillenia_WFA
                 OpenedWave = null;
             }
         }
+
+
+
+        
+
+
+
     }
 }
